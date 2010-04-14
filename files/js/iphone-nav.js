ab6c0e5aa0c6808ff34787f16a36c36ca081c297
David Sanson <david@dhcp-128-146-146-143.osuwireless.ohio-state.edu>
2010-04-14 16:10:49 -0400

    final commit on current iphone nav solution

--- files/js/iphone-nav.js
+++ files/js/iphone-nav.js
@@ -1,7 +1,6 @@
 if (window.innerWidth && window.innerWidth <= 480) { 
     $(document).ready(function(){  
-	    $('nav ul').addClass('hide'); 
-    
+	    $('nav ul li').addClass('hide'); 
         $('nav ul').prepend('<li id="menuButton" onclick="toggleMenu()">Navigation Menu</li>'); 
     });
     function toggleMenu() { 

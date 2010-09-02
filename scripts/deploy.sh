b403cee4ef6b7e4e0407d33306df336cdddb8174
David Sanson <dsanson@gmail.com>
2010-09-02 11:24:41 -0400

    lots of changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-rm -rf _site && jekyll && rsync -avz --delete _site/ nfsn:/home/public/
+rm -rf _site && \
+jekyll && \
+rsync -avz --delete _site/ nfsn:/home/public/ && \
+rsync -avz _site/ dream:/home/davsans/davidsanson.com/

e9568b5e90a85aee71591152f7ba916f2f346a1d
David Sanson <dsanson@gmail.com>
2011-03-15 22:50:06 -0400

    improved tty scripts mean I can simplify the deploy script again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 rm -rf _site && \
 jekyll && \
-./scripts/markdown_generator.sh
+#./scripts/markdown_generator.sh
 rsync -avz --delete _site/ nfsn:/home/public/
 # rsync -avz _site/ dream:/home/davsans/davidsanson.com/
